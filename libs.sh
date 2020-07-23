3589474cce03ad11715cb24598b5d651d4fac656
misanet <petrovic.n.milos@gmail.com>
2020-07-23 13:49:13 +0200

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -30,8 +30,4 @@ if [ "$DOWNLOAD" = true ]; then
     rm -f "${DEST}"
 
     echo "${VERSION}" > ${LOCK}
-    cd react-native-pjsip-builder-2.8.0
-    ./release.sh
-    cp -rf dist/ios/VialerPJSIP.framework ../ios/
-    cp -rf dist/android/src/* ../android/src
 fi
